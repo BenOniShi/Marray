5b4298344806fd0a4b06947becd41cc51b8911e4
yearcold <shibn9510@gmail.com>
2019-10-06 18:57:58 +0800

    第二次更新

---
+++ src/main/java/com/shsxt/po/Me.java
@@ -0,0 +1,11 @@
+package com.shsxt.po;
+
+import com.shsxt.Inter.Marray;
+
+public class Me implements Marray {
+
+    @Override
+    public void toMarray() {
+        System.out.println("隔壁老王结婚了");
+    }
+}
